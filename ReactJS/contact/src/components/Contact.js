3f4c1721251c702b889e757366d7b81f082f3cd2
NEW <aufcl4858@naver.com>
2017-04-11 11:59:06 +0900

    Update search function, Create react-form folder
    1) update search function with filter and indexOf
    2) create react-form folder for use when i ready to work

--- ReactJS/contact/src/components/Contact.js
+++ ReactJS/contact/src/components/Contact.js
@@ -6,6 +6,7 @@ export default class Contact extends React.Component{
   constructor(props){
     super(props);
     this.state = {
+      keyword : '',
       contactData : [{
         name:'james',
         phone :'01029393829'
@@ -20,10 +21,24 @@ export default class Contact extends React.Component{
         phone :'01029393829'
       }]
     };
+
+      this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e){
+      this.setState({
+        keyword: e.target.value
+      });
     }
 
     render(){
       const mapToComponents = (data) => {
+        data.sort();
+        data =  data.filter(
+          (contact) => {
+            return contact.name.toLowerCase().indexOf(this.state.keyword) > -1;
+          }
+        );
         return data.map((contact,i) => {
           return (<ContactInfo contact={contact} key={i}/>);
         });
@@ -32,6 +47,12 @@ export default class Contact extends React.Component{
       return(
         <div>
           <h1>Contacts</h1>
+          <input
+            name = "keword"
+            placeholder="Search"
+            value = {this.state.keyword}
+            onChange = {this.handleChange}
+          />
           <div>{mapToComponents(this.state.contactData)}</div>
         </div>
       );
