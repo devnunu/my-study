7c45a495ca2392678507e71565727e7a2bc8cc49
NEW <aufcl4858@naver.com>
2017-04-11 12:30:23 +0900

    Update contact detail view
    1) update contact detail view using react
    2) add ContactDetail.js file

--- ReactJS/contact/src/components/Contact.js
+++ ReactJS/contact/src/components/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ContactInfo from './Contactinfo';
+import ContactDetails from './ContactDetails';
 
 export default class Contact extends React.Component{
 
@@ -67,6 +68,10 @@ export default class Contact extends React.Component{
             onChange = {this.handleChange}
           />
           <div>{mapToComponents(this.state.contactData)}</div>
+          <ContactDetails 
+            isSelected={this.state.selectedKey != -1}
+            contact={this.state.contactData[this.state.selectedKey]}
+          />
         </div>
       );
     }
---
+++ ReactJS/contact/src/components/ContactDetails.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default class ContactDetails extends React.Component {
+  render(){
+
+    const details = (
+      <div>
+        <p>{this.props.contact.name}</p>
+        <p>{this.props.contact.phone}</p>
+      </div>
+    );
+    const blank = (<div>not Selected</div>);
+    return(
+      <div>
+        <h2>Details</h2>
+        {this.props.isSelected ? details : blank}
+      </div>
+    );
+  }
+}
+
+ContactDetails.defaultProps = {
+  contact:{
+    name : '',
+    phone : ''
+  }
+};
