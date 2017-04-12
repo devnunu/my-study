fac659762ae2d138862b1bea791abff5a3c54ac6
NEW <aufcl4858@naver.com>
2017-04-12 21:05:34 +0900

    [ReactJS]Update redux_example
    1) update redux_example
    2) implement todo function with input text box and button

---
+++ ReactJS/redux_example/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Todo from './Todo'
+
+class App extends React.Component {
+    render(){
+        return (
+          <Todo/>
+        );
+    }
+}
+
+export default App;
