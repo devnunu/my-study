c4e17c49b328c4a6351370a29cd2386c334c6610
NEW <aufcl4858@naver.com>
2017-04-15 18:41:08 +0900

    [React] Update contact insert/update/delete function
    1) update contact inser/update/delete function

--- ReactJS/contact/src/components/Contact.js
+++ ReactJS/contact/src/components/Contact.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ContactInfo from './Contactinfo';
 import ContactDetails from './ContactDetails';
 import update from 'react-addons-update';
+import ContactCreate from './ContactCreate';
 
 export default class Contact extends React.Component{
 
@@ -27,6 +28,36 @@ export default class Contact extends React.Component{
 
       this.handleChange = this.handleChange.bind(this);
       this.handleClick = this. handleClick.bind(this);
+      this.handleCreate = this.handleCreate.bind(this);
+      this.handleRemove = this.handleRemove.bind(this);
+      this.handleEdit = this.handleEdit.bind(this);
+    }
+
+    handleCreate(contact){
+      this.setState({
+        contactData:update(this.state.contactData,{$push:[contact]}),
+        selectedKey:-1
+      });
+    }
+
+    handleEdit(name, phone){
+      this.setState({
+        contactData:update(this.state.contactData,{
+          [this.state.selectedKey]:{
+            name:{$set:name},
+            phone:{$set:phone}
+          }
+        })
+      });
+    }
+
+    handleRemove(){
+      if(this.state.selectedKey < 0){
+        return;
+      }
+      this.setState({
+        contactData:update(this.state.contactData,{$splice:[[this.state.selectedKey,1]]})
+      });
     }
 
     handleChange(e){
@@ -72,6 +103,11 @@ export default class Contact extends React.Component{
           <ContactDetails
             isSelected={this.state.selectedKey != -1}
             contact={this.state.contactData[this.state.selectedKey]}
+            onRemove={this.handleRemove}
+            onEdit={this.handleEdit}
+          />
+          <ContactCreate
+            onCreate = {this.handleCreate}
           />
         </div>
       );
