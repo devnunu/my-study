d56d2f1118311c6bb8cee289a3d20f35565b5cee
NEW <aufcl4858@naver.com>
2017-06-10 17:41:50 +0900

    Modify Board component in tic-tac-toe game
    1. 보드 컴포넌트의 constructor를 생성하여 state 값을 배열로 선언
    
    2. Square를 만드는 renderSquare 함수도 이에 따라 변경

--- ReactJS/tic-tac-toe/src/index.js
+++ ReactJS/tic-tac-toe/src/index.js
@@ -3,18 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {this.props.value}
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+  
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]} />;
   }
 
   render() {
