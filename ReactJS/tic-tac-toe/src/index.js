13a1ba8dcb310c30ad45c26c40c38d4cc41f80e5
NEW <aufcl4858@naver.com>
2017-06-10 18:33:12 +0900

    Modify Board component in tic-tac-toe
    1. 보드 컴포넌트 내부에 handleClick 메소드 추가

--- ReactJS/tic-tac-toe/src/index.js
+++ ReactJS/tic-tac-toe/src/index.js
@@ -3,17 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>                                   
+        {this.props.value}
       </button>
     );
   }
@@ -26,9 +19,20 @@ class Board extends React.Component {
       squares: Array(9).fill(null),
     };
   }
-  
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}                                                                
+      />
+    );
   }
 
   render() {
