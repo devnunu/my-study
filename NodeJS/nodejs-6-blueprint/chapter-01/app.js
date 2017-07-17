69688d051c81b5cd3f8bbbda68f450fdf6c289f4
NEW <aufcl4858@naver.com>
2017-07-17 14:49:11 +0900

    Update views and routes folder
    1. views와 routes폴더 정리

--- NodeJS/nodejs-6-blueprint/chapter-01/app.js
+++ NodeJS/nodejs-6-blueprint/chapter-01/app.js
@@ -6,13 +6,13 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index = require('./server/routes/index');
+var users = require('./server/routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'server/views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
