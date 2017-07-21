cdeba5a4a088c476cc115aa6aff7f70b6cc6a3d3
NEW <aufcl4858@naver.com>
2017-07-21 15:02:12 +0900

    Remove router folder
    1. 라우트 폴더 제거
    
    2. app.js 내부 route 호출 제거

--- NodeJS/nodejs-6-blueprint/chapter-02/app.js
+++ NodeJS/nodejs-6-blueprint/chapter-02/app.js
@@ -27,7 +27,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', index.show);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
