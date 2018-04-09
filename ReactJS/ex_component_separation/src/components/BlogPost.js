ef020ac56b94275e5ebb0d3fbd1819174e0da0f2
eunwoo <aufcl4858@naver.com>
2018-04-09 23:59:24 +0900

    Remove unused code

--- ReactJS/ex_component_separation/src/components/BlogPost.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class BlogPost extends React.Component{
-  render(){
-    return(
-      <li onClick={this.props.onClick}>{this.props.posts.title}</li>
-    );
-  }
-}
--- ReactJS/redux/redux-auto/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import counter from './counter';
-import ui from './ui';
-
-const reducers = combineReducers({
-  counter,ui
-})
-
-export default reducers;
