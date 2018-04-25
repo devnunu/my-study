45e7e406b6abae02d95ef8d32308d152b1cb883f
eunwoo <eunwoo@finda.co.kr>
2018-04-25 14:39:04 +0900

    Update common mvp project

--- blockchain/march-mvp-version2/contracts/MetaCoin.sol
+++ blockchain/march-mvp-version2/contracts/MetaCoin.sol
@@ -6,31 +6,30 @@ pragma solidity ^0.4.18;
 // token, see: https://github.com/ConsenSys/Tokens. Cheers!
 
 // Slightly modified for 0.4.18 Solidity style. Removed the library for simplicity.
-
 contract MetaCoin {
-	mapping (address => uint) balances;
+    mapping (address => uint) balances;
 
-	function MetaCoin() public {
-		balances[msg.sender] = 10000;
-	}
+    function MetaCoin() public {
+        balances[msg.sender] = 10000;
+    }
 
-	function sendCoin(address receiver, uint amount)
-	    public
-	    returns (bool)
+    function sendCoin(address receiver, uint amount)
+        public
+        returns (bool)
     {
-		if (balances[msg.sender] < amount) {
+        if (balances[msg.sender] < amount) {
             return false;
         }
-		balances[msg.sender] -= amount;
-		balances[receiver] += amount;
-		return true;
-	}
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        return true;
+    }
 
-	function getBalance(address addr)
-	    public
-	    view
-	    returns (uint)
+    function getBalance(address addr)
+        public
+        view
+        returns (uint)
     {
-  	    return balances[addr];
-	}
+        return balances[addr];
+    }
 }
--- blockchain/march-mvp-version2/src/App.tsx
+++ blockchain/march-mvp-version2/src/App.tsx
@@ -1,11 +1,11 @@
-import * as React from "react";
-import * as Web3 from "web3";
-import getWeb3 from "./util/getWeb3";
+import * as React from 'react';
+import * as Web3 from 'web3';
+import getWeb3 from './util/getWeb3';
 
-const appStyles = require("./App.css");
-const logo = require("./logo.svg");
+const appStyles = require('./App.css');
+const logo = require('./logo.svg');
 
-import MetaWallet from "./components/MetaWallet";
+import MetaWallet from './components/MetaWallet';
 
 interface IAppState {
   web3: Web3;
@@ -30,17 +30,17 @@ class App extends React.Component<{}, IAppState> {
     return (
       <div className={appStyles.app}>
         <div className={appStyles.appHeader}>
-          <img src={logo} className={appStyles.appLogo} alt="logo" />
+          <img src={logo} className={appStyles.appLogo} alt='logo' />
           <h2>React Ethereum DApp Template</h2>
         </div>
         <div className={appStyles.appIntro}>
           {this.state.web3 ? (
             <div>
               <p>
-                Provider is MetaMask: {(this.state.web3.currentProvider as any).isMetaMask ? "yes" : "no"}
+                Provider is MetaMask: {(this.state.web3.currentProvider as any).isMetaMask ? 'yes' : 'no'}
               </p>
               <p>
-                Provider is Mist: {(window as any).mist ? "yes" : "no"}
+                Provider is Mist: {(window as any).mist ? 'yes' : 'no'}
               </p>
               {(this.state.web3.currentProvider as any).host ?
                 <p>Provider is {(this.state.web3.currentProvider as any).host}</p> : null}
