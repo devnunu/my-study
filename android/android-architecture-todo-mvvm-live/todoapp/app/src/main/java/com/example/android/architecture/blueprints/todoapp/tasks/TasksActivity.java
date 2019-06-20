5f990bb22dd9d4fd91c23c65b16210e1869bfeed
eunwoo <aufcl4858@naver.com>
2019-06-20 19:09:14 +0900

    분석용 구글 mvvm 샘플앱 추가

---
+++ android/android-architecture-todo-mvvm-live/todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksActivity.java
@@ -0,0 +1,183 @@
+/*
+ * Copyright 2016, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.architecture.blueprints.todoapp.tasks;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.android.architecture.blueprints.todoapp.Event;
+import com.example.android.architecture.blueprints.todoapp.R;
+import com.example.android.architecture.blueprints.todoapp.ViewModelFactory;
+import com.example.android.architecture.blueprints.todoapp.addedittask.AddEditTaskActivity;
+import com.example.android.architecture.blueprints.todoapp.statistics.StatisticsActivity;
+import com.example.android.architecture.blueprints.todoapp.taskdetail.TaskDetailActivity;
+import com.example.android.architecture.blueprints.todoapp.util.ActivityUtils;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+
+public class TasksActivity extends AppCompatActivity implements TaskItemNavigator, TasksNavigator {
+
+    // 전체 레이아웃이 DrawarLayout
+    private DrawerLayout mDrawerLayout;
+
+    // TasksViewModel은 액티비티 마다 여러개 사용할 수 있나??
+    private TasksViewModel mViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tasks_act);
+
+        // 메서드를 잘 파편화 해서 사용하고있다. -> 한번만 호출 되는 것임에도 가독성 측면에서 좋음
+        // 단위는 각 컴포넌트임
+        setupToolbar();
+
+        setupNavigationDrawer();
+
+        setupViewFragment();
+
+        mViewModel = obtainViewModel(this);
+
+        // Subscribe to "open task" event
+        mViewModel.getOpenTaskEvent().observe(this, new Observer<Event<String>>() {
+            @Override
+            public void onChanged(Event<String> taskIdEvent) {
+                String taskId = taskIdEvent.getContentIfNotHandled();
+                if (taskId != null) {
+                    openTaskDetails(taskId);
+                }
+
+            }
+        });
+
+        // Subscribe to "new task" event
+        mViewModel.getNewTaskEvent().observe(this, new Observer<Event<Object>>() {
+            @Override
+            public void onChanged(Event<Object> taskIdEvent) {
+                if (taskIdEvent.getContentIfNotHandled() != null) {
+                    addNewTask();
+                }
+            }
+        });
+    }
+
+    // 왜 FragmentActivity를 받지..? public이므로 여러군데에서 쓰임.
+    // AppCompatActivity는 FragmentActivity를 상속 받음
+    // 다른 곳에서 쓸때 AppCompatActivity가 아닌 FragmentActivity를 상속받아서 그런가
+    public static TasksViewModel obtainViewModel(FragmentActivity activity) {
+        // Use a Factory to inject dependencies into the ViewModel
+        ViewModelFactory factory = ViewModelFactory.getInstance(activity.getApplication());
+
+        TasksViewModel viewModel =
+                ViewModelProviders.of(activity, factory).get(TasksViewModel.class);
+
+        return viewModel;
+    }
+
+    private void setupViewFragment() {
+        TasksFragment tasksFragment =
+                (TasksFragment) getSupportFragmentManager().findFragmentById(R.id.contentFrame);
+        if (tasksFragment == null) {
+            // Create the fragment
+            tasksFragment = TasksFragment.newInstance();
+            ActivityUtils.replaceFragmentInActivity(
+                    getSupportFragmentManager(), tasksFragment, R.id.contentFrame);
+        }
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setupNavigationDrawer() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerLayout.setStatusBarBackground(R.color.colorPrimaryDark);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            setupDrawerContent(navigationView);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // Open the navigation drawer when the home icon is selected from the toolbar.
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+                            case R.id.list_navigation_menu_item:
+                                // Do nothing, we're already on that screen
+                                break;
+                            case R.id.statistics_navigation_menu_item:
+                                Intent intent =
+                                        new Intent(TasksActivity.this, StatisticsActivity.class);
+                                startActivity(intent);
+                                break;
+                            default:
+                                break;
+                        }
+                        // Close the navigation drawer when an item is selected.
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mViewModel.handleActivityResult(requestCode, resultCode);
+    }
+
+    @Override
+    public void openTaskDetails(String taskId) {
+        Intent intent = new Intent(this, TaskDetailActivity.class);
+        intent.putExtra(TaskDetailActivity.EXTRA_TASK_ID, taskId);
+        startActivityForResult(intent, AddEditTaskActivity.REQUEST_CODE);
+
+    }
+
+    @Override
+    public void addNewTask() {
+        Intent intent = new Intent(this, AddEditTaskActivity.class);
+        startActivityForResult(intent, AddEditTaskActivity.REQUEST_CODE);
+    }
+}
